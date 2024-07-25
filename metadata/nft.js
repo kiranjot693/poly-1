b39ff35b8c7c6f38367a90c95b659c27f142f8cb
Kiran Jot Singh <167282873+kiranjot693@users.noreply.github.com>
2024-07-25 17:53:37 +0530

    Create nft.js

---
+++ metadata/nft.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "sip 0",
+    image: "Qmdn3TLBPn7wbWK3kk8xCtYuVRTM7Z9werWPs12Cz6UJHH",
+  },
+  {
+    name: "1",
+    description: "ship 1",
+    image: "QmZBR9HF9sJLDpKcxYHVRjYV3aUo5YSQiS8P7twBJhRe5N",
+  }, //change
+  {
+    name: "2",
+    description: "ship 2",
+    image: "Qmd4HsuzHA8SULC8cm5jFZP8A6RNyYNuMWc7iujZWEcwFD",
+  },
+  {
+    name: "3",
+    description: "ship 3",
+    image: "Qman7hbwLgZrJW3gKxgRmRR2uMdVdrLUR8ayvf6YXedfpr",
+  },
+  {
+    name: "4",
+    description: "shiip 4",
+    image: "QmdgJaNU4oWBT7xMsegRcsguLAjwUvrodWyYBtfAGcDgAL",
+  },
+];
+
+module.exports = nfts;
